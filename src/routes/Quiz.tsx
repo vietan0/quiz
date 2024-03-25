a695162ef8d5684eb89ef809ca71129474295866
Viet An <tiktaktictac269@gmail.com>
2024-03-25 12:47:06 +0700

    revert: manually revert #b463b457

--- src/routes/Quiz.tsx
+++ src/routes/Quiz.tsx
@@ -1,10 +1,26 @@
+import { Icon } from '@iconify/react/dist/iconify.js';
+import { Button, ButtonGroup } from '@nextui-org/button';
 import { AnimatePresence, motion } from 'framer-motion';
+import { Link, useNavigate } from 'react-router-dom';
 
+import Progress from '../components/Progress';
 import QuestionScreen from '../components/QuestionScreen';
 import useMainStore from '../zustand/useMainStore';
 
 export default function Quiz() {
-  const { quiz, index, direction } = useMainStore();
+  const {
+    quiz,
+    quizErrMsg,
+    resetQuiz,
+    index,
+    direction,
+    moveIndex,
+    picked,
+    result,
+    setResult,
+  } = useMainStore();
+
+  const navigate = useNavigate();
 
   const variants = {
     enter: (direction: 1 | -1) => ({
@@ -18,20 +34,99 @@ export default function Quiz() {
     }),
   };
 
+  const errMsgDiv = (
+    <div>
+      <p>There has been an error while getting quiz:</p>
+      <pre className="text-sm text-red-500" data-testid="quizErrMsg">
+        {quizErrMsg}
+      </pre>
+      <Link to="/" className="underline" onClick={resetQuiz}>
+        Go Home
+      </Link>
+    </div>
+  );
+
+  function checkAnswers() {
+    const result = picked!.map((pick, i) => pick === quiz![i].correct_answer);
+    setResult(result);
+  }
+
   return (
-    <AnimatePresence mode="popLayout" custom={direction}>
-      <motion.div
-        key={index}
-        variants={variants}
-        custom={direction}
-        initial="enter"
-        animate="stay"
-        exit="exit"
-        transition={{ type: 'tween' }}
-        data-testid="motion.div"
-      >
-        <QuestionScreen q={quiz![index]} index={index} key={index} />
-      </motion.div>
-    </AnimatePresence>
+    <div className="flex min-h-screen flex-col justify-between">
+      {quizErrMsg ? (
+        errMsgDiv
+      ) : quiz ? (
+        <>
+          <Progress />
+          <AnimatePresence mode="popLayout" custom={direction}>
+            <motion.div
+              key={index}
+              variants={variants}
+              custom={direction}
+              initial="enter"
+              animate="stay"
+              exit="exit"
+              transition={{ type: 'tween' }}
+              data-testid="motion.div"
+            >
+              <QuestionScreen q={quiz![index]} index={index} key={index} />
+            </motion.div>
+          </AnimatePresence>
+          <div className="flex justify-between border-t px-4 py-8 xs:px-8 sm:px-16 lg:px-32">
+            <ButtonGroup radius="md">
+              <Button
+                variant="ghost"
+                size="lg"
+                className="w-40"
+                isDisabled={index === 0}
+                onPress={() => moveIndex(-1)}
+                startContent={
+                  <Icon
+                    icon="material-symbols:arrow-left-alt-rounded"
+                    width={24}
+                    height={24}
+                  />
+                }
+              >
+                Previous
+              </Button>
+              <Button
+                variant="ghost"
+                size="lg"
+                className="w-40"
+                isDisabled={index === quiz.length - 1}
+                onPress={() => moveIndex(1)}
+                endContent={
+                  <Icon
+                    icon="material-symbols:arrow-right-alt-rounded"
+                    width={24}
+                    height={24}
+                  />
+                }
+              >
+                Next
+              </Button>
+            </ButtonGroup>
+            <Button
+              size="lg"
+              isDisabled={picked!.filter((n) => n === undefined).length !== 0}
+              className="w-40 bg-success-400 font-bold text-white"
+              onPress={() => {
+                if (result) {
+                  console.log('quiz already done');
+                } else {
+                  navigate('/result');
+                  checkAnswers();
+                }
+              }}
+            >
+              {result ? 'Go To Result' : 'Submit'}
+            </Button>
+          </div>
+        </>
+      ) : (
+        'Preparing some questions...'
+      )}
+    </div>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,6 @@ import App from '../App';
 import ErrorPage from './ErrorPage';
 import Home from './Home';
 import Quiz from './Quiz';
-import QuizResultLayout from './QuizResultLayout';
 import Result from './Result';
 
 export default [
@@ -12,13 +11,8 @@ export default [
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Home /> },
-      {
-        element: <QuizResultLayout />,
-        children: [
-          { path: '/quiz', element: <Quiz /> },
-          { path: '/result', element: <Result /> },
-        ],
-      },
+      { path: '/quiz', element: <Quiz /> },
+      { path: '/result', element: <Result /> },
     ],
   },
 ];
